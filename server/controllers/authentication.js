756292a11ce3a625e93a1c5d4de635121cc6d9aa
Jacob Hoke <jacob@threads13.com>
2018-02-01 02:12:00 -0500

    initial commit

---
+++ server/controllers/authentication.js
@@ -0,0 +1,44 @@
+const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+function tokenForUser(user){
+  const timestamp = new Date().getTimp();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
+}
+
+exports.signin = function (req, res, next){
+  res.send({ token: tokenForUser(req.user) });
+}
+
+exports.signup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must supply both an email and a password'});
+  }
+
+  User.findOne({ email: email }, function(err, existingUser) {
+    if (err) {
+      return next(err);
+    }
+
+    if (existingUser){
+      return res.status(422).send({ error: 'Email is in use'});
+    }
+
+    const user = new User({
+      email: email,
+      password: password
+    });
+
+    user.save( function(err) {
+      if (err) {
+        return next (err);
+      }
+
+      res.json({token: tokenForUser(user)});
+    });
+  });
+}
